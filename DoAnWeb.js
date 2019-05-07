eb2480dee94d14599bb2ab29fe986e76af5fb61b
SonPham <phamngocsonl13.cla@gmail.com>
2019-05-07 13:41:19 +0700

    Sửa hiệu ứng trang chủ độc giả
    Người đăng:Phạm Ngọc Sơn
    Nhiệm vụ: Sửa va thêm một số hiệu ứng trang chủ

--- DoAnWeb.js
+++ DoAnWeb.js
@@ -6,6 +6,20 @@ $(document).ready(function() {
 	$(".btn-sumnit-user").click(function(event) {
 		/* Act on the event */
 		$(".NavbarUser").toggle(300);
-	
+
 	});
+	$(".btn-submit-search").click(function(event) {
+		/* Act on the event */
+		event.preventDefault(); 
+		$("#search-text-div").fadeToggle(400);
+	});
+	$(window).scroll(function(){
+		if ($(this).scrollTop()>300) {
+			$("#back-to-top").fadeIn(300);
+		}
+		else{
+			$("#back-to-top").fadeOut(300);
+		}
+	});
+
 });
