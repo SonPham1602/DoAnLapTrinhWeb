c2a3e3e14a22104bec010e8eabd69e9409040364
huason030298 <45062934+huason030298@users.noreply.github.com>
2019-06-05 14:55:05 +0700

    Update lại truy vấn sql
    Người đăng: Hứa Thanh Sơn

--- models/article.model.js
+++ models/article.model.js
@@ -8,15 +8,17 @@ module.exports = {
 		return db.load(`select * from article where id = ${id}`)
 	},
 	allByCat: idcat => {
-		return db.load(`select a.*, count(c.id) as num_comment from article a left join comment c 
-						on id_cat = ${idcat} and c.id_art = a.id
-						group by a.id`)
+		// return db.load(`select a.*, count(c.id) as num_comment from article a left join comment c 
+		// 				on id_cat = ${idcat} and c.id_art = a.id
+		// 				group by a.id`)
+		return db.load(`select * from article where id_cat = ${idcat}`)
 	},
 	
 	allByCat2:  idcat2 => {
-		return db.load(`select a.*, count(c.id) as num_comment from article a left join comment c 
-						on id_cat2 = ${idcat2} and c.id_art = a.id
-						group by a.id;`)
+		// return db.load(`select a.*, count(c.id) as num_comment from article a left join comment c 
+		// 				on id_cat2 = ${idcat2} and c.id_art = a.id
+		// 				group by a.id;`)
+		return db.load(`select * from article where id_cat2 = ${idcat2}`)
 	}
 	
 }
