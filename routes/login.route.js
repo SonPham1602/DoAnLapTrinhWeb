03ba9fb30c84bbfd9b19361524953d5e30dbbf0e
SonPham1602 <phamngocsonl13.cla@gmail.com>
2019-06-18 02:38:56 +0700

    Sửa lại trang admin
    Người đăng:Phạm Ngọc Sơn

--- routes/login.route.js
+++ routes/login.route.js
@@ -9,7 +9,7 @@
 // module.exports = router;
 
 var express = require('express');
-// var passport = require('passport'); phải bật cmd lên npm install passport này
+var passport = require('passport'); //phải bật cmd lên npm install passport này
 //const userModel = require('../../models/user.model'); lên google sear ch cài lại này là đc
 var router = express.Router();
 
